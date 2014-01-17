3fcc3febb089f84c52bed1cabd732a00b942858c
momphis <momphis.just-dice@gmail.com>
2014-01-17 10:36:34 +0000

    Added modal, dialog tests

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,6 +1,12 @@
 $(function() {
     
-;
-$( ".draggable" ).draggable();
+
+$( ".dialog" ).dialog( { 'title' : 'Survey Test: Dialog #1', 'closeText' : 'x' } );
     
+$( ".newDialog" ).click( function ( e ) { 
+    var d = document.createElement( 'div' );
+    $( d ).dialog( { 'title' : 'Dialog #'+ parseInt( ( $( ".ui-dialog" ).length )+1 ) });
+    $( d ).text( 'we can make as many of them as we want' );
+});
+
 });
